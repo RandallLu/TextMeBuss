55a296480006e83b93ea48820fded818634debb5
alexcys <choyuansu@gmail.com>
2017-10-22 00:53:21 -0700

    Fix Access Error

--- rest.py
+++ rest.py
@@ -11,6 +11,7 @@ app = web.application(urls, globals())
 
 class get_route:
     def GET(self, route):
+		web.header('Access-Control-Allow-Origin', '*')
 		return json.dumps(busstime.get_stop_list(route))
 
 if __name__ == "__main__":
